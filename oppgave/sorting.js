87a06e1a86ecc205bde30a4ce2681f169fb8428e
Roman Krupa <kruparoman33@gmail.com>
2026-09-03 13:15:51 +0200

    SortNumbers funker

--- oppgave/sorting.js
+++ oppgave/sorting.js
@@ -8,33 +8,23 @@
 
 // Del A: tall, minst først
 // sortNumbers([5, 3, 9, 1, 7]) -> [1, 3, 5, 7, 9]
+sortNumbers([5, 3, 9, 1, 7]);
 function sortNumbers(list) {
-  // TODO: lag en kopi av list
-  // TODO: flytt elementene til de står i riktig rekkefølge
-  // TODO: returner kopien
-  return undefined; // slett denne når du begynner
+  let arr = [...list];
+  let endret;
+
+  do {
+    endret = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+        endret = true;
+      }
+    }
+  } while (endret);
+  return arr;
 }
 
-// Del B: ord i norsk alfabetrekkefølge
-// sortWords(["ål", "ære", "øy"]) -> ["ære", "øy", "ål"]
-//
-// Kopierte du del A og byttet bare ut sammenligningen? Da står sorteringa
-// to steder. Det eneste som skiller dem, er hvordan to elementer
-// sammenlignes. Kan du gi algoritmen den forskjellen utenfra?
-function sortWords(list) {
-  // TODO
-  return undefined; // slett denne når du begynner
-}
-
-// Del C: både tall og ord
-// 1. tall før ord  2. tall minst først  3. ord alfabetisk
-// sortMixed(["banan", 7, "and", 7, -1]) -> [-1, 7, 7, "and", "banan"]
-//
-// Tips: typeof a === "number" sier om noe er et tall.
-function sortMixed(list) {
-  // TODO
-  return undefined; // slett denne når du begynner
-}
 
 if (typeof module !== "undefined") {
   module.exports = { sortNumbers, sortWords, sortMixed };
